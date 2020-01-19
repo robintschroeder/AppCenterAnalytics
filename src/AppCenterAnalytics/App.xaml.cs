b06c1a2b524736407d4dd84657cd0bf9be22c57c
robintschroeder <robintschroeder@gmail.com>
2020-01-19 12:38:09 -0600

    init commit

---
+++ src/AppCenterAnalytics/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AppCenterAnalytics.Services;
+using AppCenterAnalytics.Views;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace AppCenterAnalytics
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("ios=a7a2d1ad-07ab-43e2-a9cb-7fb8b7242cc7;" +
+                  "uwp={Your UWP App secret here};" +
+                  "android=b3f14a4f-f414-4c26-bf1a-d0808076fec7",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ src/AppCenterAnalytics/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AppCenterAnalytics.Models;
+
+namespace AppCenterAnalytics.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
